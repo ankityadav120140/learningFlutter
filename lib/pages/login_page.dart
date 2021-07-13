33f6b3947d654f698055c60e3634d18d34179123
ankityadav120140 <ankityadav120140@gmal.com>
2021-07-13 23:53:20 +0530

    day5

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 
 class loginPage extends StatefulWidget {
   const loginPage({Key? key}) : super(key: key);
@@ -14,68 +15,86 @@ class _loginPageState extends State<loginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/login.png",
-              scale: 4,
-              fit: BoxFit.cover,
-            ),
-            Text(
-              "Welocome",
-              textScaleFactor: 3,
-              style: TextStyle(
-                color: Colors.black87,
-                // fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+      body: SafeArea(
+        child: Material(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset(
+                        "assets/login.png",
+                        scale: 4,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text(
+                      "Welocome",
+                      textScaleFactor: 3,
+                      style: TextStyle(
+                        color: Colors.black87,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                        ),
+                        hintText: "Enter User Name",
+                        labelText: "User Name",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                        ),
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => homePage()),
+                          );
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 28,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(),
+                      ),
+                    ),
+                  ],
                 ),
-                hintText: "Enter User Name",
-                labelText: "User Name",
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                ),
-                hintText: "Enter Password",
-                labelText: "Password",
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 28,
-                  ),
-                ),
-                style: TextButton.styleFrom(),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
